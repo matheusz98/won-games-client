a0a08e6f8dac8272075890852f1ad7c60b175407
Matheus Costa <matheuscastiglia31@gmail.com>
2022-03-11 16:46:42 -0300

    Adicionando ícones ao botão

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,18 +1,31 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 import Button from '.'
 
 export default {
-    title: 'Button',
-    component: Button,
-    argTypes: {
-        children: {
-            type: 'string'
-        }
+  title: 'Button',
+  component: Button,
+
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {
+        type: 'symbol'
     }
+  }
+
 } as Meta
 
 export const Default: Story = (args) => <Button {...args} />
-
 Default.args = {
-    children: 'Buy now'
+  children: 'Buy now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />
 }
