3f3b81f98774a692068811a3e19f484459e2856a
Matheus Costa <matheuscastiglia31@gmail.com>
2022-03-14 11:14:18 -0300

    Estilizando o MenuFull

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Logo from 'components/Logo'
+import Button from 'components/Button'
 import * as S from './styles'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
@@ -28,6 +29,20 @@ const Menu = () => {
 
             <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
                 <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+                <S.MenuNav>
+                    <S.MenuLink href="#">Home</S.MenuLink>
+                    <S.MenuLink href="#">Explore</S.MenuLink>
+                </S.MenuNav>
+
+                <S.RegisterBox>
+                    <Button fullWidth size="large">
+                        Log in now
+                    </Button>
+                    <span>or</span>
+                    <S.CreateAccount href="#" title="Sign Up">
+                        Sign Up
+                    </S.CreateAccount>
+                </S.RegisterBox>
             </S.MenuFull>
         </S.Wrapper>
     )
