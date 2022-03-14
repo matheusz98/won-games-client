93554247fca69acbdb290d71332345ac6a032e55
Matheus Costa <matheuscastiglia31@gmail.com>
2022-03-14 11:54:40 -0300

    Regra para usuário autenticado

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,7 +7,11 @@ import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 
-const Menu = () => {
+export type MenuProps = {
+    username?: string
+}
+
+const Menu = ({ username }: MenuProps) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
@@ -32,17 +36,26 @@ const Menu = () => {
                 <S.MenuNav>
                     <S.MenuLink href="#">Home</S.MenuLink>
                     <S.MenuLink href="#">Explore</S.MenuLink>
+
+                    {!!username && (
+                        <>
+                            <S.MenuLink href="#">My Account</S.MenuLink>
+                            <S.MenuLink href="#">Wishlist</S.MenuLink>
+                        </>
+                    )}
                 </S.MenuNav>
 
-                <S.RegisterBox>
-                    <Button fullWidth size="large">
-                        Log in now
-                    </Button>
-                    <span>or</span>
-                    <S.CreateAccount href="#" title="Sign Up">
-                        Sign Up
-                    </S.CreateAccount>
-                </S.RegisterBox>
+                {!username && (
+                    <S.RegisterBox>
+                        <Button fullWidth size="large">
+                            Log in now
+                        </Button>
+                        <span>or</span>
+                        <S.CreateAccount href="#" title="Sign Up">
+                            Sign Up
+                        </S.CreateAccount>
+                    </S.RegisterBox>
+                )}
             </S.MenuFull>
         </S.Wrapper>
     )
