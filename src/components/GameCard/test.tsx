f205e2581884df2a96aadd34abda558b759506b0
Matheus Costa <matheuscastiglia31@gmail.com>
2022-03-17 11:22:38 -0300

    Criando a função de favoritar

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
@@ -31,14 +32,38 @@ describe('<GameCard />', () => {
     })
 
     it('should render price in label', () => {
-        // renderiza o componente
-        // preço não tenha line-through
-        // preço tenha o background secundário
-      })
-    
-      it('should render a line-through in price when promotional', () => {
-        // renderiza o componente (COM promotionalPrice) // 200 reais // 15 reais
-        // preço tenha line-through (200)
-        // preço novo promocional não vai ter line-through (15)
-      })
+        renderWithTheme(<GameCard {...props} />)
+
+        const price = screen.getByText('R$ 235,00')
+
+        expect(price).not.toHaveStyle({ textDecoration: 'line-through' })
+        expect(price).toHaveStyle({ backgroundColor: theme.colors.secondary })
+    })
+
+    it('should render a line-through in price when promotional', () => {
+        renderWithTheme(<GameCard {...props} promotionalPrice="R$ 15,00" />)
+
+        expect(screen.getByText('R$ 235,00')).toHaveStyle({
+            textDecoration: 'line-through'
+        })
+
+        expect(screen.getByText('R$ 15,00')).not.toHaveStyle({
+            textDecoration: 'line-through'
+        })
+    })
+
+    it('should render a filled Favorite icon when favorite is true', () => {
+        renderWithTheme(<GameCard {...props} favorite />)
+
+        expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument()
+    })
+
+    it('should call onFav method when favorite is clicked', () => {
+        const onFav = jest.fn()
+        renderWithTheme(<GameCard {...props} favorite onFav={onFav} />)
+
+        fireEvent.click(screen.getAllByRole('button')[0])
+
+        expect(onFav).toBeCalled()
+    })
 })
