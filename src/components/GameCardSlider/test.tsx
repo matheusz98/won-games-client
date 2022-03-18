2598740d570f44d9963f3b36214c11dbf601fcdf
Matheus Costa <matheuscastiglia31@gmail.com>
2022-03-18 17:41:58 -0300

    Estilizando o GameCardSlider

--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -1,4 +1,5 @@
 import 'match-media-mock'
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCardSlider from '.'
@@ -52,5 +53,16 @@ describe('<GameSlider />', () => {
     it('should render with 4 active items', () => {
         const { container } = renderWithTheme(<GameCardSlider items={items} />)
         expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+    })
+
+    it('should render white arrows if color passed', () => {
+        renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+        expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+            color: '#FAFAFA'
+        })
+        expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+            color: '#FAFAFA'
+        })
     })
 })
